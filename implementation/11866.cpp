8fa0b34405ebaebbed05b26c379888fa3c94c32f
suengwone <52214766+suengwone@users.noreply.github.com>
2022-01-02 18:37:16 +0900

    Create 11866.cpp

---
+++ implementation/11866.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void){
+    int N,K, cur_index = 0;
+    scanf("%d %d",&N,&K);
+    
+    vector<int> arr(N), answer;
+    
+    for(int i=0;i<N;i++){
+        arr[i] = i+1;
+    }
+    
+    int count;
+    while(answer.size() != N){
+        count = 1;
+        while(true){
+            if(cur_index == arr.size()){
+                cur_index = 0;
+            }
+            
+            if(count == K){
+                answer.push_back(arr[cur_index]);
+                arr.erase(arr.begin()+cur_index);
+                break;
+            }
+            cur_index += 1;
+            count += 1;
+        }
+    }
+    
+    printf("<");
+    for(int i=0;i<N;i++){
+        printf("%d", answer[i]);
+        if(i != N-1){
+            printf(", ");
+        } else {
+            printf(">\n");
+        }
+    }
+}
