a7027e9e492fbca1333d238e875d92303cdcbdf7
suengwone <52214766+suengwone@users.noreply.github.com>
2022-08-07 17:01:11 +0900

    Update *2470.cpp

--- sorting/*2470.cpp
+++ sorting/*2470.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int N, result = 2000000000;
 
+// 2022.02.15
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -46,4 +47,69 @@ int main(void) {
 	}
 
 	cout << res_idx_1 << " " << res_idx_2 << '\n';
+}
+
+// 2022.08.07
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    
+    int N;
+    cin >> N;
+
+    vector<int> liqList = vector<int>(N);
+
+    for(int i=0; i<N; i++)
+    {
+        cin >> liqList[i];
+    }
+
+    sort(liqList.begin(),liqList.end());
+
+    auto front = liqList.begin();
+    auto back = liqList.rbegin();
+
+    int base = 2000000001;
+    pair<int,int> answer = pair<int,int>(*front, *back);
+
+    auto fisrtIt = liqList.begin();
+    auto lastIt = liqList.rbegin();
+
+    while(true)
+    {
+        int tmp = *front + *back;
+
+        if(base > abs(tmp))
+        {
+            answer = pair<int,int>(*front, *back);
+            base = abs(tmp);
+        }
+
+        if(tmp > 0)
+        {
+            back++;
+            if(front - fisrtIt + 1 + back - lastIt >= N)
+            {
+                break;
+            }
+        }
+        else if(tmp < 0)
+        {
+            front++;
+            if(front - fisrtIt + 1 + back - lastIt >= N)
+            {
+                break;
+            }
+        }
+        else
+        {
+            answer = pair<int,int>(*front, *back);
+            break;
+        }
+    }
+
+    cout << answer.first << " " << answer.second << '\n';
+
+    return 0;
 }
