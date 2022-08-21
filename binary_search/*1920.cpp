b898125a5e5e760aac4dd83382dd59eaab95e465
suengwone <52214766+suengwone@users.noreply.github.com>
2022-08-21 16:25:27 +0900

    Update *1920.cpp

--- binary_search/*1920.cpp
+++ binary_search/*1920.cpp
@@ -1,4 +1,4 @@
-// 수 찾기
+// 수 찾기 첫번째 풀이
 
 #include <iostream>
 #include <algorithm>
@@ -46,4 +46,37 @@ int main(void){
         scanf("%d", &find_key);
         solution(N, find_key);
     }
+}
+
+// 수 찾기 두번째 풀이
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    int N, M,temp;
+    set<long long int> CheckSet;
+    
+    cin >> N;
+    for(int i=0; i<N; i++)
+    {
+        cin>>temp;
+        CheckSet.insert(temp);
+    }
+    
+    cin >> M;
+    int searchNum;
+    for(int i=0; i<M; i++)
+    {
+        cin >> searchNum;
+        CheckSet.find(searchNum) == CheckSet.end() ? cout << 0 : cout << 1;
+        cout << '\n';
+    }
+    
+    return 0;
 }
