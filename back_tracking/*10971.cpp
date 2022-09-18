ba0d5e27f9ec3a19ca233f12821a2fc151810de4
suengwone <52214766+suengwone@users.noreply.github.com>
2022-09-18 17:12:17 +0900

    Update *10971.cpp

--- back_tracking/*10971.cpp
+++ back_tracking/*10971.cpp
@@ -1,4 +1,4 @@
-// 외판원 순회 2
+// 외판원 순회 2 첫번째 풀이
 
 #include <iostream>
 
@@ -44,4 +44,78 @@ int main(void) {
     }
 
     printf("%d\n",m);
+}
+
+
+// 외판원 순회 2 두번째 풀이
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+
+unsigned int minAnswer = 100000000;
+
+vector< vector<unsigned int> > arr;
+
+void TravelVillage(int curVille, vector<bool> check, int curValue, int startVille, int travelVilleCount)
+{
+    check[curVille] = true;
+
+    if(travelVilleCount == N)
+    {
+        if(arr[curVille][startVille] == 0)
+        {
+            return;
+        }
+        else
+        {
+            curValue += arr[curVille][startVille];
+
+            if(curValue < minAnswer)
+            {
+                minAnswer = curValue;
+            }
+
+            return;
+        }
+    }
+    else
+    {
+        for(int i=0; i<N; i++)
+        {
+            if(check[i] != true && arr[curVille][i] != 0)
+            {
+                TravelVillage(i, check, curValue + arr[curVille][i], startVille, travelVilleCount + 1);
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> N;
+
+    arr = vector< vector<unsigned int> >(N, vector<unsigned int>(N));
+
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<N; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    vector<bool> checkArr = vector<bool>(N,false);
+
+    for(int i=0; i<N; i++)
+    {
+        TravelVillage(i, checkArr, 0, i, 1); 
+    }
+
+    cout << minAnswer << '\n';
+
+    return 0;
 }
