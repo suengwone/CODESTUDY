93d658f91919d44335f37987a28294abcc50a0c9
suengwone <52214766+suengwone@users.noreply.github.com>
2022-09-23 22:09:44 +0900

    Update ?2580.cpp

--- back_tracking/?2580.cpp
+++ back_tracking/?2580.cpp
@@ -1,131 +1,101 @@
 // 스도쿠
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int board[9][9];
+int sdokuBoard[9][9];
+vector< pair<int,int> > problem;
+int cnt;
+bool found = false;
 
-void col_check(int num, vector<int> check) {
-	int cnt = 0, pos = -1, value = 0;
-	for (int i = 0; i < 9; i++) {
-		if (board[i][num] == 0) {
-			pos = i;
-			continue;
-		}
-		else {
-			check[board[i][num]] = true;
-			cnt++;
-		}
-	}
+bool Check(int x, int y)
+{
+    for(int i=0; i<9; i++)
+    {
+        if(sdokuBoard[x][i] == sdokuBoard[x][y] && i != y)
+        {
+            return false;
+        }
+        if(sdokuBoard[i][y] == sdokuBoard[x][y] && i != x)
+        {
+            return false;
+        }
+    }
 
-	if (cnt == 9) return;
+    int square_x = x / 3, square_y = y / 3;
+    for(int i = square_x * 3; i < square_x * 3 + 3; i++)
+    {
+        for(int j = square_y * 3; j < square_y * 3 + 3; j++)
+        {
+            if(sdokuBoard[i][j] == sdokuBoard[x][y])
+            {
+                if(i != x || j != y)
+                {
+                    return false;
+                }
+            }
+        }
+    }
 
-	for (int i = 1; i < 10; i++) {
-		if (check[i] == false) {
-			value = i;
-			break;
-		}
-	}
-
-	if (cnt == 8) {
-		board[pos][num] = value;
-	}
-
+    return true;
 }
 
-void row_check(int num, vector<int> check) {
-	int cnt = 0, pos = -1, value = 0;
-	for (int i = 0; i < 9; i++) {
-		if (board[num][i] == 0) {
-			pos = i;
-			continue;
-		}
-		else {
-			check[board[num][i]] = true;
-			cnt++;
-		}
-	}
+void Sdoku(int Num)
+{
+    if(Num == cnt)
+    {
+        for(int i=0; i<9; i++)
+        {
+            for(int j=0; j<9; j++)
+            {
+                cout<< sdokuBoard[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+        found = true;
+        return;
+    }
 
-	if (cnt == 9) return;
-
-	for (int i = 1; i < 10; i++) {
-		if (check[i] == false) {
-			value = i;
-			break;
-		}
-	}
-
-	if (cnt == 8) {
-		board[num][pos] = value;
-	}
+    for(int i=1; i<=9; i++)
+    {
+        sdokuBoard[problem[Num].first][problem[Num].second] = i;
+        if(Check(problem[Num].first, problem[Num].second))
+        {
+            Sdoku(Num+1);
+        }
+        if(found)
+        {
+            return;
+        }
+    }
+    
+    sdokuBoard[problem[Num].first][problem[Num].second] = 0;
+    return;
 }
 
-void box_check(int num, vector<int> check) {
-	int cnt = 0, value=0, col = num / 3, row = num % 3;
-	int col_pos, row_pos;
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (board[col*3 + i][row*3 + j] == 0) {
-				col_pos = col*3 + i, row_pos = row*3 + j;
-				continue;
-			}
-			else {
-				check[board[col*3 + i][row*3 + j]] = true;
-				cnt++;
-			}
-		}
-	}
 
-	if (cnt == 9) return;
+    for(int i=0; i<9; i++)
+    {
+        for(int j=0; j<9;j++)
+        {
+            cin >> sdokuBoard[i][j];
+            if(sdokuBoard[i][j] == 0)
+            {
+                problem.push_back(pair<int,int>(i,j));
+            }
+        }
+    }
 
-	for (int i = 1; i < 10; i++) {
-		if (check[i] == false) {
-			value = i;
-			break;
-		}
-	}
+    cnt = problem.size();
 
-	if (cnt == 8) {
-		board[col_pos][row_pos] = value;
-	}
+    Sdoku(0);
 
-}
-
-int main(void) {
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			scanf("%d", &board[i][j]);
-		}
-	}
-
-	vector<int> check(10,0);
-	bool isClear = true;
-
-	do {
-		int cnt = 0;
-		for (int i = 0; i < 9; i++) {
-			row_check(i, check);
-			col_check(i, check);
-			box_check(i, check);
-		}
-
-		for (int i = 0; i < 9; i++) {
-			if (find(board[i], board[i] + 9, 0) == &board[i][9])
-				cnt += 1;
-		}
-
-		if (cnt == 9)
-			isClear = false;
-	} while (isClear);
-
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			printf("%d ", board[i][j]);
-		}
-		printf("\n");
-	}
+    return 0;
 }
