4fca771c9572fc9469ab9d4924f24ec14391112e
suengwone <52214766+suengwone@users.noreply.github.com>
2023-03-12 20:15:01 +0900

    Update ?1012.cpp

--- graph_theory/?1012.cpp
+++ graph_theory/?1012.cpp
@@ -66,3 +66,71 @@ int main()
         }
     }
 }
+
+// 23.3.12
+#include <iostream>
+#include <memory.h>
+
+using namespace std;
+
+bool check[51][51];
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+int field[51][51];
+int M,N,K;
+
+void DFS(int x, int y)
+{
+    check[x][y] = true;
+    for(int i=0; i<4; i++)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx >= 0 && nx < N && ny >= 0 && ny < M)
+        {
+            if(field[nx][ny] == 1 && check[nx][ny] == false)
+            {
+                DFS(nx,ny);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int T;
+
+    cin >> T;
+
+    while(T--)
+    {
+        cin >> M >> N >> K;
+
+        int x, y, count = 0;
+
+        for(int i=0; i<K; i++)
+        {
+            cin >> x >> y;
+            field[y][x] = 1;
+        }
+
+        for(int i=0; i<N; i++)
+        {
+            for(int j=0; j<M; j++)
+            {
+                if(field[i][j] == 1 && check[i][j] == false)
+                {
+                    count += 1;
+                    DFS(i,j);
+                }
+            }
+        }
+        
+        cout << count << '\n';
+
+        memset(check,false,sizeof(check));
+        memset(field,0, sizeof(field));
+    }
+}
