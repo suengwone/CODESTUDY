f3ad890329810a436381f0bbf1746e0d355dcac2
suengwone <52214766+suengwone@users.noreply.github.com>
2023-03-21 21:46:42 +0900

    Create Product_of_Array_Except_Self.cpp

---
+++ NeetCode150/Arrays&Hashing/Product_of_Array_Except_Self.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> answer(size);
+        
+        answer[0] = 1;
+
+        for(int i=1; i<size; i++)
+        {
+            answer[i] = answer[i-1] * nums[i-1];
+        }
+
+        int right = 1;
+        for(int i=size-1; i>=0; i--)
+        {
+            answer[i] *= right;
+            right *= nums[i];
+        }
+
+        return answer;
+    }
+};
